4d6b6b043396567c4a3f0b1d71cca608a07ddae1
403-33-phurinut <54728@yupparaj.ac.th>
2026-08-03 13:37:25 +0700

    Update extras.py

--- extras.py
+++ extras.py
@@ -8,6 +8,23 @@ st.divider()
 
 
 st.header("2.1 work")
-code = '''def hello():
-    print("Hello, Streamlit!")'''
+code = '''import streamlit as st
+st.title("แอปพลิเคชั่นแปลง พ.ศ. เป็น ค.ศ.")
+st.caption("มีการดัดแปลง")
+
+value = 543
+bhYear = int(st.number_input("กรอกปี พ.ศ.") - value)
+ceYear = int(st.number_input("กรอกปี ค.ศ.") + value)
+if bhYear > 0:
+  ceResult = bhYear
+elif bhYear <= 0:
+  ceResult = f"ก่อนคริสต์ศักราช {abs(bhYear)} ปี"
+
+if ceYear > 0:
+  bhResult = ceYear
+elif ceYear <= 0:
+  bhResult = f"ก่อนพุทธศักราช {abs(ceYear)} ปี"
+  
+st.subheader(f"ปี ค.ศ. คือ: {ceResult}")
+st.subheader(f"ปี พ.ศ. คือ: {bhResult}")'''
 st.code(code, language="python")
