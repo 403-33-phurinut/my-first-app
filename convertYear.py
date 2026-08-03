46fbbda11340f4285f17d9742017eb2d4fc0404e
403-33-phurinut <54728@yupparaj.ac.th>
2026-08-03 13:07:26 +0700

    Update convertYear.py

--- convertYear.py
+++ convertYear.py
@@ -8,12 +8,12 @@ ceYear = st.number_input("กรอกปี ค.ศ.") - value
 if bhYear > 0:
   ceResult = bhYear
 elif bhYear <= 0:
-  ceResult = f"ก่อนคริสต์ศักราช abs({bhYear}) ปี"
+  ceResult = f"ก่อนคริสต์ศักราช {abs(bhYear)} ปี"
 
 if ceYear > 0:
   bhResult = ceYear - value
 elif ceYear <= 0:
-  bhResult = f"ก่อนคริสต์ศักราช abs({ceYear}) ปี"
+  bhResult = f"ก่อนคริสต์ศักราช {abs(ceYear)} ปี"
   
 st.subheader(f"ปี ค.ศ. คือ: {ceResult}")
 st.subheader(f"ปี พ.ศ. คือ: {bhResult}")
