43b7ae6b9d284e57e94d406529a0fe626003f4ee
403-33-phurinut <54728@yupparaj.ac.th>
2026-08-03 13:25:14 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -3,7 +3,7 @@ st.title("🛒แอปพลิเคชั่นคำนวณราคาส
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
 net_price = price - vat
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.subheader(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.subheader(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
-st.write("นาย **ภูริณัฐ บ่อไทย** เลขที่ **33**  ม.**4/3**")
+st.write("นาย ***ภูริณัฐ บ่อไทย*** เลขที่ ***33***  ม.***4/3***")
