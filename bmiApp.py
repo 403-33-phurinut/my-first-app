271f5cdeec23d59afdb886e24fb0f7a43cc7e804
403-33-phurinut <54728@yupparaj.ac.th>
2026-08-10 13:21:31 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.markdown("# :red[:material/wc: คำนวณค่าดัชนีมวลกาย BMI]")
+st.markdown("# :red[***:material/wc: คำนวณค่าดัชนีมวลกาย BMI***]")
+st.subheader("*กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น*")
