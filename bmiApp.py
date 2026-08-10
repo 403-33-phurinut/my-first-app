89a916c5112e5dff1e53ca0f598a1bdc5a682760
403-33-phurinut <54728@yupparaj.ac.th>
2026-08-10 13:25:50 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.markdown("# :red[***:material/wc: คำนวณค่าดัชนีมวลกาย BMI***]")
-st.divider()
 st.write("*กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น*")
+st.divider()
 
 weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):",min_value=0.0)
 height = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):",min_value=0.0)
