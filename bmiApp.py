ecd76a967708b50d137268aded73cf544cbba37f
403-33-phurinut <54728@yupparaj.ac.th>
2026-08-17 08:43:03 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -13,12 +13,12 @@ if st.button("**:material/info:** คำนวณ BMI"):
   st.write(f"### ค่าดัชนีมวลกาย/BMI ของคุณคือ: ***{bmi:.2f} kg/m²***")
 
   if bmi < 18.5:
-    st.badge("คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)",icon=:material/warning)
+    st.badge("คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)",icon=:material/warning:)
   elif 18.5 <= bmi < 23.0:
-    st.badge("คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)",icon=:material/check)
+    st.badge("คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)",icon=:material/check:)
   elif 23.0 <= bmi < 25.0:
-    st.badge("คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)",icon=:material/warning)
+    st.badge("คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)",icon=:material/warning")
   else:
-    st.badge("คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย,icon=":material/dangerous)
+    st.badge("คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย,icon=:material/dangerous:)
 st.divider()
 st.write("นาย ภูริณัฐ บ่อไทย เลขที่ 33 ม.4/3")
