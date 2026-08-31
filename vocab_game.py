b47d5af6a1f65f029a4ca240a4b76a385f82b9a2
403-33-phurinut <54728@yupparaj.ac.th>
2026-08-31 13:15:49 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -30,17 +30,17 @@ def show_result_dialog(ans1, ans2):
 
     # ตรวจข้อ 1
     if u_ans1 == "apple":
-        st.success("✅ ข้อ 1: ถูกต้อง")
+        st.success(":material/check: ข้อ 1: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
+        st.error(f":material/close: ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
     # ตรวจข้อ 2
     if u_ans2 == "fish":
-        st.success("✅ ข้อ 2: ถูกต้อง")
+        st.success(":material/check: ข้อ 2: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f":material/close: ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
 
