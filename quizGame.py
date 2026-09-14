270f58381e1fce085e44aeb3dd967f3d8880b2e0
403-33-phurinut <54728@yupparaj.ac.th>
2026-09-14 13:00:15 +0700

    Update quizGame.py

--- quizGame.py
+++ quizGame.py
@@ -14,3 +14,9 @@ ans1Temp = ""
 ans2Temp = ""
 ans3Temp = ""
 ans4Temp = ""
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
